8c38a1b225ac805bbde97afa9674fc4e2009fcb3
Andrey Sapranovich <SapranovichAndrey@yandex.ru>
2020-03-01 22:21:56 +0300

    feat:edit singolo1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 $(function(){
 
-	$('.slider-inner').slick({
-		nextArrow: '<button type="button" class="slick-btn slick-next">❯</button>',
-		prevArrow: '<button type="button" class="slick-btn slick-prev">❮</button>',
-
-	});
+$('.slider-inner, .news-slider-inner').slick({
+	 nextArrow:'<button type="button" class="slick-btn slick-next">❯</button>',
+	 prevArrow:'<button type="button" class="slick-btn slick-prev">❮</button>',
+	 infinite: false
+});
 
 });
